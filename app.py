5ae6ee9cc3c885192b2ecf7cc4c9ac05ccbcaa8a
Federickg23 <federickg23@gmail.com>
2020-05-03 22:17:07 -0400

    Added the 1006 filepath

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 Created on Tue Apr 21 14:57:17 2020
 
 @author: etill
+Name: Federick Gonzalez
+Uni:  fag2113
 
 """
 
@@ -15,7 +17,11 @@ app = Flask(__name__)
 #static route
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route("/1006")
+def classPage():
+    return render_template("1006.html")
 
 #start the server
 if __name__ == "__main__":
